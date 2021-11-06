a41271cad4817c457f4693d8149a99bc1c66cd95
Cansnow <261001642@qq.com>
2021-11-06 20:41:26 +0800

    Update pay.js

--- templets/plus/eacpay/pay.js
+++ templets/plus/eacpay/pay.js
@@ -6,12 +6,13 @@ $(function() {
         jQuery.getJSON('/plus/eacpay/home.php?dopost=check&orderid=' + orderid, function(d) {
             if (d.code == "1") {
                 clearInterval(timeId);
+                jQuery('#eacpayresult .loading .bar').css('width','100%');
                 $('#eacpayresult .resultmsg').html("充值成功");
                 setTimeout(function() {
                     location.href = "/plus/eacpay/home.php?dopost=success";
                 }, 2000);
             }else if (d.code == "2") {
-                jQuery('#eacpayresult .loading .bar').css('width',(d.confirmations/d.receiptConfirmation)+'%');
+                jQuery('#eacpayresult .loading .bar').css('width',(parseInt(d.confirmations)/parseInt(d.receiptConfirmation))*100+'%');
                 jQuery('#eacpayresult .resultmsg').html('正在确认订单，请稍等...');
             }else if (d.code == "3") {
                 jQuery('#eacpayresult .loading .bar').css('width','100%');
@@ -21,6 +22,7 @@ $(function() {
                 jQuery('#eacpayresult .resultmsg').html(d.msg);
             } else {
                 clearInterval(timeId);
+                jQuery('#eacpayresult .loading .bar').css('width','100%');
                 jQuery('#eacpayresult .resultmsg').html(d.msg);
             }
         });
